18ded958da70935c6204b9bc9ab485264d4a939e
klthao <thaok07@my.saintpaul.edu>
2017-03-03 00:40:24 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -37,23 +37,10 @@
             }
           }
         };
-		
-	bot.commands.amyCommand = {
-          command: 'amy',
-          rank: 'user',
-          type: 'exact',
-          functionality: function (chat, cmd) {
-            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-            if (!bot.commands.executable(this.rank, chat)) return void (0);
-            else {
-              API.sendChat("All Hail Amy!");
-            }
-          }
-        };
 	    
 	bot.commands.debCommand = {
           command: 'deb',
-          rank: 'user',
+          rank: 'host',
           type: 'exact',
           functionality: function (chat, cmd) {
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
